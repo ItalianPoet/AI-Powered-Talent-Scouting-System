74d247bbbc4b93f169f118842a7e980cb7c7ad58
ItalianPoet <119521543+ItalianPoet@users.noreply.github.com>
2025-01-17 21:59:39 +0530

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import UploadVideo from "./components/UploadVideo";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>AI Talent Scouting System</h1>
+      <UploadVideo />
+    </div>
+  );
+}
+
+export default App;
